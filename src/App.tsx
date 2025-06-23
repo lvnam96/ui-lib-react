fc2b2504746d779c94f45a6de106772a388b6202
lvnam96 <12493778+lvnam96@users.noreply.github.com>
2025-06-23 23:14:00 +0700

    fix(dev): add Tailwind directive to `src/App.tsx` to bundle Tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { BlinkBlink, FancyBox } from './lib/components';
+import { BlinkBlink, FancyBox, Pikachu } from './lib/components';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,16 +8,21 @@ function App() {
   return (
     <>
       <div className="flex">
-        <div className="w-1/2">
+        <div className="w-md-1/2">
           <FancyBox />
         </div>
-        <div className="w-1/2">
+        <div className="w-md-1/2">
           <BlinkBlink
             counter={count}
             onClick={() => {
               setCount((count) => count + 1);
             }}
           />
+        </div>
+      </div>
+      <div className="flex">
+        <div className="w-md-1/2">
+          <Pikachu />
         </div>
       </div>
     </>
