4164234df2b3c467d568e66edf673d59a9e20585
lvnam96 <12493778+lvnam96@users.noreply.github.com>
2025-06-23 07:54:09 +0700

    build: update `eslint` config

--- eslint.config.js
+++ eslint.config.js
@@ -6,18 +6,25 @@ import tseslint from 'typescript-eslint';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import tsParser from '@typescript-eslint/parser';
+import eslintReact from '@eslint-react/eslint-plugin';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default tseslint.config(
   { ignores: ['node_modules', 'dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      eslintReact.configs['recommended-typescript'],
+    ],
     files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: './tsconfig.app.json',
+        project: './tsconfig.app.json', // OR: ['./tsconfig.node.json', './tsconfig.app.json'],
+        projectService: true, // for better TypeScript integration
         tsconfigRootDir: import.meta.dirname || __dirname,
       },
       ecmaVersion: 2020,
